06f8563a4286400acadf8622a981a83b232f9315
NateC85 <90124755+NateC85@users.noreply.github.com>
2021-12-06 11:48:12 -0700

    Update Inheritance.py
    Fixes

--- Assignments/Inheritance.py
+++ Assignments/Inheritance.py
@@ -1,20 +1,22 @@
 
 
-
+#parent class
 class Computers:
     CPU = True
     connects_internet = True
     operating_system = "must have"
     PSU = True
     
-    
+#child class    
 class Desktop(Computers):
+    #unique attributes
     screen_size = "wide range"
     input_devices = "external"
     
     
-
-class Laptop(computers):
+#child class
+class Laptop(Computers):
+    #unique attributes
     has_touchpad = True
     has_mobility = True
     
