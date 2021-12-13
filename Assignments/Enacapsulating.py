a3b0f41de76dbf0b42effe204565003438a1ae8e
NateC85 <90124755+NateC85@users.noreply.github.com>
2021-12-12 21:48:46 -0700

    Update Enacapsulating.py

--- Assignments/Enacapsulating.py
+++ Assignments/Enacapsulating.py
@@ -1,17 +1,16 @@
 
 
-
-from typing import AsyncGenerator
-
-
+#Initializing the class and setting the protected and private variables
 class User:
-     def __init__(self, name, age=0):
+     def __init__(self, name, age=7):
          self._name = name
          self.__age = age
 
+# Accessing variables
      def display(self):
-         print(self._name)
-         print(self.__age)
+         print("Name: ", self._name)
+         print("Age: ", self.__age)
+
 
      def Name(self):
          self._name = ""
@@ -19,13 +18,15 @@ class User:
      def Age(self):
          print(self.__age)
 
+# defining the getter method
      def getAge(self):
          print(self.__age)
 
+# defining the setter method
      def setAge(self, age):
-         self.__age = "Age is 7 years old"
+         self.__age = ""
 
-
+# Calling the functions
 user = User("Jovanni")
 user.display()
 user.setAge(7)
