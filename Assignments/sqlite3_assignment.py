14cf084779870477d14ed4b029b2a290781ef68f
Nathan Crawford <natec85@gmail.com>
2022-02-13 08:16:56 -0700

    Delete Assignments directory

--- Assignments/sqlite3_assignment.py
+++
@@ -1,8 +0,0 @@
-
-
-
-import sqlite3
-connection = sqlite3.connect("C:/Users/natec/Desktop/test_database.db")
-
-c = connection.cursor()
-c.execute("CREATE TABLE People(FirstName TEXT, LastName TEXT, Age INT)")
